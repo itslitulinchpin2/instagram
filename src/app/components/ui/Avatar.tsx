cfc2024d137b1f35588993ec3a310f401b077f52
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-04-30 11:10:19 +0200

    user Profile Styling

--- src/app/components/ui/Avatar.tsx
+++ src/app/components/ui/Avatar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react'
 
-type AvatarSize = 'small' | 'large'|'medium';
+type AvatarSize = 'small' | 'large'|'medium' | 'xlarge';
 type Props = {
     image ?: string | null;
     size?: AvatarSize;
@@ -12,6 +12,8 @@ function getImageSizeStyle(size:AvatarSize):string{
     case 'small': return 'w-[34px] h-[34px]  p-[0.1rem]'
     case 'medium': return 'w-[42px] h-[42px]  p-[0.1rem]'
     case 'large': return 'w-16 h-16 p-[0.2rem]'
+    case 'xlarge' : return 'w-[138px] h-[138px]  p-[0.3rem]'
+    default: throw new Error(`Unsupported type size : ${size}`)
 
   }
 }
@@ -21,6 +23,8 @@ function getContainerSize(size:AvatarSize):string{
     case 'small': return 'w-9 h-9'
     case 'medium': return 'w-11 h-11'
     case 'large': return 'w-[68px] h-[68px]'
+    case 'xlarge': return 'w-[142px] h-[142px]'
+    default: throw new Error(`Unsupported type size : ${size}`)
 
   }
 }
--- src/app/components/ui/UserProfile.tsx
+++ src/app/components/ui/UserProfile.tsx
@@ -15,18 +15,21 @@ export default function UserProfile({user}:Props) {
     {title:'followings', data:following}
   ]  
   return (
-    <section>
-        <Avatar image={image} highlight/>
-        <div>
-            <h1>{username}</h1>
+    <section className='w-full flex flex-col md:flex-row items-center justify-center py-12 border-b border-neutral-300'>
+        <Avatar image={image} highlight size='xlarge'/>
+        <div className='md:ml-10 basis-1/3'>
+          <div className='flex flex-col md:flex-row items-center '>
+            <h1 className='text-2xl md:mr-8 my-2 md:mb-0'>{username}</h1>
             <FollowButton user={user}/>
-            <ul>
+          </div>
+            
+            <ul className='my-4 flex gap-4'>
                 {info.map(({title,data}, index) => <li key={index}>
-                    <span>{data}</span>
+                    <span className='font-bold mr-1'>{data}</span>
                     {title}
                 </li> )}
             </ul>
-            <p>{name}</p>
+            <p className='text-xl font-bold text-center md:text-start'>{name}</p>
         </div>
     </section>
   )
