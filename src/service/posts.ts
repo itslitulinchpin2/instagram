640225fca6c32d5a8da2a5ae847e662f52890de8
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-05-01 00:25:02 +0200

    modify function getSavedPostOf()

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -66,14 +66,15 @@ export async function getLikedPostsOf(username:string){
         `
     ).then(mapPosts)}
 
-export async function getSavedPostsOf(username:string){
-    return client.fetch(
-        `
-        *[_type=="post" && _id in *[_type=="user" && username="${username}"].bookmarks[]._ref]
-        | order(_createdAt desc){
-            ${simplePostProjection}
-        }
-        `
-    ).then(mapPosts)}
+    export async function getSavedPostsOf(username: string) {
+        return client
+          .fetch(
+            `*[_type == "post" && _id in *[_type=="user" && username=="${username}"].bookmarks[]._ref]
+            | order(_createdAt desc){
+              ${simplePostProjection}
+            }`
+          )
+          .then(mapPosts);
+      }
     
 
